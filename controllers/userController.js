c4ab9ee0a41a7ff8f313d0d35e63d99af735f6ff
Garrick Ngai <garrickdark@yahoo.com>
2017-09-18 14:45:40 -0700

    added fonts and more layout for pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,23 @@ function index(req, res) {
   })
 }
 
+function create(req, res) {
+  // create new comment with form data (`req.body`)
+    console.log('comment created');
+    var newComment = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    }
+
+    user = new db.user(newUser);
+
+    user.save(function handleDBUserSaved(err, savedUser) {
+      res.json(savedUser);
+    });
+  };
+
 module.exports = {
   index: index,
+  create: create
 };
--- public/scripts/bunnydefense/Boot.js
+++ public/scripts/bunnydefense/Boot.js
@@ -9,19 +9,19 @@ Bunnydefense.Boot.prototype = {
     },
 
     create: function() {
-        this.input.maxPointers = 1;
-		this.stage.disableVisibilityChange = false;
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.minWidth = 270;
-		this.scale.minHeight = 480;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-		this.stage.forcePortrait = true;
-		this.scale.setScreenSize(true);
+      this.input.maxPointers = 1;
+  		this.stage.disableVisibilityChange = false;
+  		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  		this.scale.minWidth = 270;
+  		this.scale.minHeight = 480;
+  		this.scale.pageAlignHorizontally = true;
+  		this.scale.pageAlignVertically = true;
+  		this.stage.forcePortrait = true;
+  		this.scale.setScreenSize(true);
 
-		this.input.addPointer();
-		this.stage.backgroundColor = '#171642';
+  		this.input.addPointer();
+  		this.stage.backgroundColor = '#171642';
 
-        this.state.start('Preloader');
+      this.state.start('Preloader');
     }
 }
